47182eba3ea87ce55ea547d2ebc29df2459a9e12
Beth Secor <elizabeth.a.secor@gmail.com>
2016-04-18 15:15:47 -0600

    User can select unread links or all links

--- spec/features/user_can_filter_links_by_statuses_spec.rb
+++ spec/features/user_can_filter_links_by_statuses_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "UserCanFilterLinksByStatuses", type: :feature do
-  it "should filter links on the page when entering search term", js: true do
+  it "should filter links on the page when selecting unread only", js: true do
     user = create(:user)
     user.links.create(title: "JavaScript for Cats", url: "http://jsforcats.com/", read: true)
     user.links.create(title: "Cats", url: "https://www.reddit.com/r/cats/")
@@ -16,5 +16,38 @@ RSpec.feature "UserCanFilterLinksByStatuses", type: :feature do
 
     expect(page).to have_content("Cats")
     expect(page).to have_content("Flowers")
+  end
+
+  it "should filter links on the page when selecting read only", js: true do
+    user = create(:user)
+    user.links.create(title: "JavaScript for Cats", url: "http://jsforcats.com/")
+    user.links.create(title: "Cats", url: "https://www.reddit.com/r/cats/")
+    user.links.create(title: "Flowers", url: "http://www.bonniebraeflowers.com/", read: true)
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+
+    visit '/'
+
+    select "Read", :from => "link_status[filter]"
+
+    expect(page).to have_content("Flowers")
+
+    expect(page).to_not have_content("Cats")
+    expect(page).to_not have_content("JavaScript for Cats")
+  end
+
+  it "should show all links when selecting blank option", js: true do
+    user = create(:user)
+    user.links.create(title: "JavaScript for Cats", url: "http://jsforcats.com/")
+    user.links.create(title: "RedditCats", url: "https://www.reddit.com/r/cats/")
+    user.links.create(title: "Flowers", url: "http://www.bonniebraeflowers.com/", read: true)
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+
+    visit '/'
+
+    select "", :from => "link_status[filter]"
+
+    expect(page).to have_content("Flowers")
+    expect(page).to have_content("RedditCats")
+    expect(page).to have_content("JavaScript for Cats")
   end
 end
