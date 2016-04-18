d23081d895cee0158bcc5e9eaccd50ec443424c5
Beth Secor <elizabeth.a.secor@gmail.com>
2016-04-18 09:57:34 -0600

    Test for unique email on user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a unique email" do
+    user = create(:user, email: "beth@turing.io")
+    new_user = User.new(email: "beth@turing.io", password: "123")
+    expect(new_user.invalid?)
+  end
 end
