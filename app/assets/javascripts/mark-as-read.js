650aa2e2120d0e2bcddea5671e0091ead3c5ad4e
Beth Secor <elizabeth.a.secor@gmail.com>
2016-04-18 13:54:25 -0600

    User can mark link as read

--- app/assets/javascripts/mark-as-read.js
+++ app/assets/javascripts/mark-as-read.js
@@ -1,3 +1,27 @@
 function markAsRead() {
-  
+  $("#links-list").delegate("#read", 'click', function() {
+    var $button = $(this);
+    var $link = $button.closest(".link");
+
+    var linkRead = {
+      link: {
+        read: true
+      }
+    };
+
+    $.ajax({
+      type: "PUT",
+      url: "/api/v1/links/" + $link.attr('data-id') + ".json",
+      data: linkRead,
+      success: function(updateLink) {
+        $link.attr('data-read', "true");
+        $link.removeClass("read-false");
+        $link.addClass("read-true");
+        $button.replaceWith("<button name='unread' id='unread'>Mark As Unread</button>");
+      },
+      error: function(xhr) {
+        console.log(xhr.responseText);
+      }
+    });
+  });
 }
---
+++ spec/features/user_can_mark_link_as_unreads_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.feature "UserCanMarkLinkAsUnreads", type: :feature do
+  pending "add some scenarios (or delete) #{__FILE__}"
+end
