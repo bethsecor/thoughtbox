3112aefdc50a91aeaf0b441b5c43347adbc79d1b
Beth Secor <elizabeth.a.secor@gmail.com>
2016-04-18 15:27:36 -0600

    Test for sorting links alphabetically

---
+++ spec/features/user_can_sort_links_alphabeticallies_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "UserCanSortLinksAlphabeticallies", type: :feature do
+  it "should filter links on the page when selecting unread only", js: true do
+    user = create(:user)
+    user.links.create(title: "JavaScript for Cats", url: "http://jsforcats.com/", read: true)
+    user.links.create(title: "Cats", url: "https://www.reddit.com/r/cats/")
+    user.links.create(title: "Flowers", url: "http://www.bonniebraeflowers.com/")
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+
+    visit '/'
+
+    click_on "Sort"
+
+    within("#links-list div:nth-child(1)") do
+      expect(page).to have_content("Cats")
+    end
+    within("#links-list div:nth-child(2)") do
+      expect(page).to have_content("Flowers")
+    end
+      within("#links-list div:nth-child(3)") do
+      expect(page).to have_content("JavaScript for Cats")
+    end
+  end
+end
