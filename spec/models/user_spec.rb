76d110067c3d9ee8a3a55c1cb55a76a90a4180ac
Beth Secor <elizabeth.a.secor@gmail.com>
2016-04-18 10:20:51 -0600

    Test for user model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe User, type: :model do
 
     expect(new_user).to be_invalid
   end
+
+  it "should have email" do
+    new_user = User.new(password: "123")
+
+    expect(new_user).to be_invalid
+  end
+
+  it "should have password" do
+    new_user = User.new(email: "beth@turing.io")
+
+    expect(new_user).to be_invalid
+  end
+
+  it "should have matching password and password confirmation" do
+    new_user = User.new(email: "beth@turing.io", password: "123", password_confirmation: "1234")
+
+    expect(new_user).to be_invalid
+  end
 end
