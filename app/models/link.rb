88128f147d9efed0d9e103c54f0ffb93ac1e8b79
Beth Secor <elizabeth.a.secor@gmail.com>
2016-04-18 11:37:07 -0600

    Link must have a title

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,5 @@ class Link < ActiveRecord::Base
   belongs_to :user
 
   validates :url, presence: true, url: true
+  validates :title, presence: true
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Link, type: :model do
 
     expect(link).to be_invalid
   end
+
+  it 'should have a url' do
+    link = Link.new(title: "Cats")
+
+    expect(link).to be_invalid
+  end
+
+  it 'should have a title' do
+    link = Link.new(url: "https://www.reddit.com/r/cats/")
+
+    expect(link).to be_invalid
+  end
 end
