c2ae9bc25aa3829ab24f4ce3fb71576500904e1d
Beth Secor <elizabeth.a.secor@gmail.com>
2016-04-18 13:57:28 -0600

    Test for marking link as unread

--- spec/features/user_can_mark_link_as_unreads_spec.rb
+++ spec/features/user_can_mark_link_as_unreads_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "UserCanMarkLinkAsUnreads", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it "can mark a link as unread" do
+    user = create(:user_with_link)
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+    link = user.links.first
+    link.update(read: true)
+
+    visit '/'
+
+
+    within("#link-#{link.id}") do
+      click_on "Mark As Unread"
+      expect(page).to have_content("Mark As Read")
+    end
+    expect(page).to have_css(".read-false")
+  end
 end
